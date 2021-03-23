07f0f3e646f24471dde9bd1d436fe58f08801e52
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 05:54:20 +0100

    Create AchievementPage.xaml.cs

---
+++ FitnessDiary.Uwp.App/Views/AchievementPage.xaml.cs
@@ -0,0 +1,18 @@
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FitnessDiary.Uwp.App.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AchievementPage : Page
+    {
+        public AchievementPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
