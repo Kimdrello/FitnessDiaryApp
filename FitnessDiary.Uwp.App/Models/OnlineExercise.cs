9f7fc84f1e576e464e82c20c882c5c3b79e5998d
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 05:49:27 +0100

    Create OnlineExercise.cs

---
+++ FitnessDiary.Uwp.App/Models/OnlineExercise.cs
@@ -0,0 +1,18 @@
+using FitnessDiary.Model;
+
+namespace FitnessDiary.Uwp.App.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OnlineExercise
+    {
+        /// <summary>
+        /// Gets or sets the exercise.
+        /// </summary>
+        /// <value>
+        /// The exercise.
+        /// </value>
+        public Exercise Exercise { get; set; }
+    }
+}
