d3ea8fb488da5896e7a312ae14f363b60c922377
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 05:49:44 +0100

    Add files via upload

---
+++ FitnessDiary.Uwp.App/Models/WorkoutEvent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using FitnessDiary.Model;
+using System.Collections.Generic;
+
+namespace FitnessDiary.Uwp.App.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class WorkoutEvent
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        /// <value>
+        /// The title.
+        /// </value>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date time title.
+        /// </summary>
+        /// <value>
+        /// The date time title.
+        /// </value>
+        public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sessions.
+        /// </summary>
+        /// <value>
+        /// The sessions.
+        /// </value>
+        /// CA2227, This cant be readonly, the set is being used.
+        public virtual List<Session> Sessions { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether [not old].
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [not old]; otherwise, <c>false</c>.
+        /// </value>
+        public bool NotOld { get; set; }
+    }
+}
