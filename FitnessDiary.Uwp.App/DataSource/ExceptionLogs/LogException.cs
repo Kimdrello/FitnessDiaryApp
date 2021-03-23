b86df3cb6a27c58dbc0c01f10e62d89b30a70a7a
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 05:45:01 +0100

    Add files via upload

---
+++ FitnessDiary.Uwp.App/DataSource/ExceptionLogs/LogException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FitnessDiary.Model;
+using System.Diagnostics;
+
+namespace FitnessDiary.Uwp.App.DataSource.ExceptionLogs
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LogException
+    {
+        /// <summary>
+        /// Logs the specified exception.
+        /// </summary>
+        /// <param name="exception">The exception.</param>
+        public async void Log(Exception exception)
+        {
+            ExceptionLog exceptionLog = new ExceptionLog()
+            {
+                ExceptionMessage = exception.Message.ToString(),
+                ExceptionSource = exception.StackTrace.ToString(),
+                ExceptionType = exception.Source.GetType().Name.ToString(),
+                ExceptionID = Guid.NewGuid(),
+                DateOfException = DateTime.Today
+            };
+
+            try
+            {
+                if (await ExceptionLogs.Instance.AddExceptionLog(exceptionLog))
+                    Debug.WriteLine("Exception added to log");
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("Exception was not logged, see Error: " + ex.Message);
+            }
+        }
+    }
+}
