a1ae70d15d56fb76949e1e3b6390827f7a559ebb
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 05:43:30 +0100

    Add files via upload

---
+++ FitnessDiary.Uwp.App/DataSource/Bodytypes/Result.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FitnessDiary.Uwp.App.DataSource.Bodytypes
+{
+    /// <summary>
+    /// Generated trough json2csharp.com to access bodytypes in the GET Request to the REST API https://wger.de/en/software/api
+    /// </summary>
+    public class Result
+    {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+    }
+}
---
+++ FitnessDiary.Uwp.App/DataSource/Bodytypes/RootObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace FitnessDiary.Uwp.App.DataSource.Bodytypes
+{
+    /// <summary>
+    /// Generated trough json2csharp.com to access RootObject of the GET Request to the REST API https://wger.de/en/software/api
+    /// </summary>
+    public class RootObject
+    {
+        /// <summary>
+        /// Gets or sets the count.
+        /// </summary>
+        /// <value>
+        /// The count.
+        /// </value>
+        public int Count { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next.
+        /// </summary>
+        /// <value>
+        /// The next.
+        /// </value>
+        public object Next { get; set; }
+
+        /// <summary>
+        /// Gets or sets the previous.
+        /// </summary>
+        /// <value>
+        /// The previous.
+        /// </value>
+        public object Previous { get; set; }
+
+        /// <summary>
+        /// Gets or sets the results.
+        /// </summary>
+        /// <value>
+        /// The results.
+        /// </value>
+        /// CA2227, The get is being used when data is being pulled from the online API
+        public List<Result> Results { get; set; }
+    }
+}
