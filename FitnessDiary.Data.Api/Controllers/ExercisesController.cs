f4ed758b1fe56ac2ded5f42bb433f20194741ad3
Kim-Andre <31319299+Kimdrello@users.noreply.github.com>
2021-03-23 06:00:43 +0100

    Add files via upload

---
+++ FitnessDiary.Data.Api/Controllers/ExercisesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FitnessDiary.DataAccess;
+using FitnessDiary.Model;
+
+namespace FitnessDiary.Data.Api.Controllers
+{
+    public class ExercisesController : ApiController
+    {
+        private FitnessDiaryContext db = new FitnessDiaryContext();
+
+        // GET: api/Exercises
+        public IQueryable<Exercise> GetExercises()
+        {
+            return db.Exercises;
+        }
+
+        // GET: api/Exercises/5
+        [ResponseType(typeof(Exercise))]
+        public async Task<IHttpActionResult> GetExercise(Guid id)
+        {
+            Exercise exercise = await db.Exercises.FindAsync(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(exercise);
+        }
+
+        // PUT: api/Exercises/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutExercise(Guid id, Exercise exercise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != exercise.ExerciseID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(exercise).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Exercises
+        [ResponseType(typeof(Exercise))]
+        public async Task<IHttpActionResult> PostExercise(Exercise exercise)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Exercises.Add(exercise);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ExerciseExists(exercise.ExerciseID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = exercise.ExerciseID }, exercise);
+        }
+
+        // DELETE: api/Exercises/5
+        [ResponseType(typeof(Exercise))]
+        public async Task<IHttpActionResult> DeleteExercise(Guid id)
+        {
+            Exercise exercise = await db.Exercises.FindAsync(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            db.Exercises.Remove(exercise);
+            await db.SaveChangesAsync();
+
+            return Ok(exercise);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ExerciseExists(Guid id)
+        {
+            return db.Exercises.Count(e => e.ExerciseID == id) > 0;
+        }
+    }
+}
---
+++ FitnessDiary.Data.Api/Controllers/SessionsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FitnessDiary.DataAccess;
+using FitnessDiary.Model;
+
+namespace FitnessDiary.Data.Api.Controllers
+{
+    public class SessionsController : ApiController
+    {
+        private FitnessDiaryContext db = new FitnessDiaryContext();
+
+        // GET: api/Sessions
+        public IQueryable<Session> GetSessions()
+        {
+            return db.Sessions;
+        }
+
+        // GET: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> GetSession(Guid id)
+        {
+            Session session = await db.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(session);
+        }
+
+        // PUT: api/Sessions/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutSession(Guid id, Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != session.SessionID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(session).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SessionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Sessions
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> PostSession(Session session)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Sessions.Add(session);
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (SessionExists(session.SessionID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = session.SessionID }, session);
+        }
+
+        // DELETE: api/Sessions/5
+        [ResponseType(typeof(Session))]
+        public async Task<IHttpActionResult> DeleteSession(Guid id)
+        {
+            Session session = await db.Sessions.FindAsync(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            db.Sessions.Remove(session);
+            await db.SaveChangesAsync();
+
+            return Ok(session);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SessionExists(Guid id)
+        {
+            return db.Sessions.Count(e => e.SessionID == id) > 0;
+        }
+    }
+}
